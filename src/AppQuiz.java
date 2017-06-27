f6d6ef6c84d763804b4a3a8e987c30355065f6e8
Yoshiki Nakamura <yoshiki.nakamura.sys@gmail.com>
2017-06-27 12:20:35 +0900

    first commit

---
+++ src/AppQuiz.java
@@ -0,0 +1,30 @@
+
+public class AppQuiz {
+  int point;
+  int total;
+  public static void main(String[] args) {
+    AppQuiz aq = new AppQuiz();
+    aq.init();
+  }
+  public void init(){
+
+  }
+  public int question1(){
+    return point;
+  }
+  public int question2(){
+    return point;
+  }
+  public int question3(){
+    return point;
+  }
+  public int question4(){
+    return point;
+  }
+  public int question5(){
+    return point;
+  }
+  public int question6(){
+    return point;
+  }
+}
