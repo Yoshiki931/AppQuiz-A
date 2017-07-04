56c80052b42324c5474672503e5bc92b718d46a2
anbo takuma <takuma.anbo.sys@gmail.com>
2017-07-04 10:44:52 +0900

    あんぽ

--- src/AppQuiz.java
+++ src/AppQuiz.java
@@ -25,6 +25,26 @@ public class AppQuiz {
     return point;
   }
   public int question6(){
+	  int point = 0;
+
+		System.out.print("質問6");
+		System.out.print("”CG”とは何の略？");
+		System.out.print("1.コンピュータ・グラフィック  2.コーポレーション・グランド  3.コピー・グラデーション");
+
+		System.out.println("解答:" + point = sc.nextInt());
+		if(point == 1) {
+			System.out.println("正解");
+		}
+		else if(point == 2){
+			System.out.println("不正解");
+		}
+		else if(point == 3){
+			System.out.println("不正解");
+		}
+		else{
+			System.out.println("1～3のもじを入力してください");
+		}
+
     return point;
   }
 }
