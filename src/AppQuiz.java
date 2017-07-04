385d816c95d443f69b607c5880a18d74bde64cd5
Yuria Sato <yuria.sato.sys@gmail.com>
2017-07-04 10:12:45 +0900

    問2実装

--- src/AppQuiz.java
+++ src/AppQuiz.java
@@ -13,6 +13,17 @@ public class AppQuiz {
     return point;
   }
   public int question2(){
+	  int ans = 0;
+	  System.out.println("ホッキョクウサギの特徴とは？");
+	  System.out.println("1.泳ぐことができる 2.3メートル以上ジャンプする 3.肉を食べる 4.時速100km以上で走る");
+	  System.out.print("回答：");
+	  ans = sc.nextInt();
+	  if(ans == 3) {
+		  System.out.println("正解！");
+		  point = 2;
+	  }else {
+		  System.out.println("不正解");
+	  }
     return point;
   }
   public int question3(){
